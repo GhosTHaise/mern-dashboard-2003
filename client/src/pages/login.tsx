063c01aea1471e834a8632560b56a11fa5c98ba6
GhosTHaise <Ghostrex2@gmail.com>
2023-03-01 19:32:43 +0300

    Connect Login to Google

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -20,7 +20,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: "your-client-id",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID || "",
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
