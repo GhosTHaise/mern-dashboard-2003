835041522a04eedffcaab7bf99039e73a820c91e
GhosTHaise <Ghostrex2@gmail.com>
2023-03-05 20:04:17 +0300

    create bar charts

--- client/src/components/charts/TotalRevenue.tsx
+++ client/src/components/charts/TotalRevenue.tsx
@@ -23,7 +23,7 @@ const TotalRevenue = (props: Props) => {
           Total Revenu
       </Typography>
       <Stack
-      my={20} 
+      my={"20px"} 
       direction="row"
       gap={4}
       flexWrap="wrap"   
@@ -35,7 +35,34 @@ const TotalRevenue = (props: Props) => {
           >
               $236,575
           </Typography>
+          <Stack 
+          direction="row"
+          alignItems="center"
+          gap={1}
+          >
+              <ArrowCircleUpRounded 
+                sx={{
+                  fontSize : 25,
+                  color : "#475be8"
+                }}
+              />
+              <Stack>
+                  <Typography fontSize={15} color={"#475be8"}>
+                     0.8%
+                  </Typography>
+                  <Typography fontSize={12} color={"#808191"}>
+                     Than Last Month
+                  </Typography>
+              </Stack>
+          </Stack>
       </Stack>
+
+      <ReactApexChart
+        series={TotalRevenueSeries}
+        type="bar"
+        height={310}
+        options={TotalRevenueOptions}
+      />
     </Box>
   )
 }
