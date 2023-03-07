431a13b3b9c01318d62acb307ebd2982d781b097
GhosTHaise <Ghostrex2@gmail.com>
2023-03-07 18:38:32 +0300

    Create personal form

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,10 +1,80 @@
 import React from 'react'
+import { Box,Typography,FormControl,FormHelperText,
+TextField,TextareaAutosize,Stack,Select,MenuItem,Button } from '@pankod/refine-mui'
 
-type Props = {}
+import { FormProps } from 'interfaces/common'
+import CustomButton from './CustomButton'
 
-const Form = (props: Props) => {
+const Form = ({
+  type,
+  register,
+  handleSubmit,
+  handleImageChange,
+  formLoading,
+  onFinishHandler,
+  propertyImage}: FormProps) => {
   return (
-    <div>Form</div>
+    <Box>
+        <Typography 
+        textTransform="capitalize"
+        fontSize={25}
+        fontWeight={700}
+        color="#11142d"
+        >
+            {type} a Property
+        </Typography>
+        <Box
+        mt={2.5}
+        borderRadius="15px"
+        padding="20px"
+        bgcolor="#fcfcfc"
+        >
+          <form 
+          style={{
+            marginTop : "20px",
+            width:"100%",
+            display:"flex",
+            flexDirection : "column",
+            gap : "20px"
+          }}
+          onSubmit={handleSubmit(onFinishHandler)}
+          >
+            <FormControl>
+              <FormHelperText sx={{
+                fontWeight : 50,
+                margin : "10px",
+                fontSize : 16,
+                color : "#11142d"
+              }}>
+                Enter property name
+              </FormHelperText>
+              <TextField 
+              fullWidth
+              required
+              id='outlined-basic'
+              color='info'
+              variant='outlined'
+              {...register("title",{
+                required : true
+              })}
+              />
+            </FormControl>
+            <FormControl>
+              <FormHelperText sx={{
+                fontWeight : 50,
+                margin : "10px",
+                fontSize : 16,
+                color : "#11142d"
+              }}>
+                Enter Description
+              </FormHelperText>
+              <TextareaAutosize
+                
+              />
+            </FormControl>
+          </form>
+        </Box>
+    </Box>
   )
 }
 
--- client/src/pages/create-property.tsx
+++ client/src/pages/create-property.tsx
@@ -10,8 +10,22 @@ const CreateProperty = (props: Props) => {
   const {data : user} = useGetIdentity();
   const [propertyImage, setPropertyImage] = useState({name : "",url : ""});
   const {refineCore : {onFinish,formLoading},register,handleSubmit} = useForm()
+  
+  const handleImageChange = () => {};
+
+  const onFinishHandler = () => {};
+
   return (
-    <div>create-property</div>
+    <Form 
+      type="create"
+      register={register}
+      onFinish={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      propertyImage={propertyImage}
+    />
   )
 }
 
