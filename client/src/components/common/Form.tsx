bf59ad1f2a8a2ec6cf15f6a586e5c9c60eff3b29
GhosTHaise <Ghostrex2@gmail.com>
2023-03-09 18:39:53 +0300

    create button to form and async function to get image from input

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -203,7 +203,21 @@ const Form = ({
                         />
                     </Button>
                 </Stack>
+                <Typography 
+                fontSize={14}
+                color="#808191"
+                sx={{
+                  wordBreak : "break-all"
+                }}
+                >
+                  {propertyImage?.name}
+                </Typography>
             </Stack>
+            <CustomButton
+            type='submit'
+            title={formLoading ? "Submitting..." : "Submit"}
+            backgroundColor={'#475be8'} 
+            color={'#fcfcfc'} />
           </form>
         </Box>
     </Box>
