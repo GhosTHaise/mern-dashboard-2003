c09e4edda9f7c00bf28babc49b6bbe681299cfbd
GhosTHaise <Ghostrex2@gmail.com>
2023-03-10 21:41:55 +0300

    create controllers

--- server/routes/property.routes.js
+++ server/routes/property.routes.js
@@ -1,1 +1,18 @@
 import express from "express";
+import {
+    getAllProperties,
+    getPropertyDetail,
+    createProperty,
+    updateProperty,
+    deleteProperty
+} from "../controllers/property.controller.js";
+
+const Router = express.Router();
+
+Router.route("/").get(getAllProperties);
+Router.route("/:id").get(getPropertyDetail);
+Router.route("/").post(createProperty);
+Router.route("/").patch(updateProperty);
+Router.route("/").delete(deleteProperty);
+
+export default Router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,1 +1,15 @@
 import express from "express";
+
+import {
+    getAllUsers,
+    createUser,
+    getUserInfoByID
+} from "../controllers/user.controller.js"
+
+const Router = express.Router();
+
+Router.route("/").get(getAllUsers);
+Router.route("/").post(createUser);
+Router.route("/:id").post(getUserInfoByID);
+
+export default Router;
