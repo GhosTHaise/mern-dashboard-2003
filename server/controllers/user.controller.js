5a9cbafa98dfec4585709688b7330779782ad4c4
GhosTHaise <Ghostrex2@gmail.com>
2023-03-10 21:27:29 +0300

    create controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,1 +1,13 @@
 import User from "../mongodb/models/user.js"
+
+const getAllUsers = async (req,res) => {};
+const createUser = async (req,res) => {};
+const getUserInfoByID = async (req,res) => {};
+
+//const updateUserInfoByID = async (req,res) => {};
+
+export {
+    getAllUsers,
+    createUser,
+    getUserInfoByID
+}
