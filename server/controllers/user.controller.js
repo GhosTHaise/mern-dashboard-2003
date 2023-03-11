87424becbfc9eaa4b43fdedc22b60ace330840b9
GhosTHaise <Ghostrex2@gmail.com>
2023-03-11 19:32:15 +0300

    Build create user function to save user info to db

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,27 @@
 import User from "../mongodb/models/user.js"
 
 const getAllUsers = async (req,res) => {};
-const createUser = async (req,res) => {};
+
+const createUser = async (req,res) => {
+    try {
+        const {name,email,avatar} = req.body;
+
+    const userExists = await User.findOne({email});
+
+    if(userExists) return res.status(200).json(userExists);
+
+    const newUser = await User.create({ 
+        name,
+        email,
+        avatar
+    });
+
+    return res.status(200).json(newUser);
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+};
+
 const getUserInfoByID = async (req,res) => {};
 
 //const updateUserInfoByID = async (req,res) => {};
