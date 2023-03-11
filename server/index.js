b14574abcb740dda11bc48a7f5795ff3ecd1c06f
GhosTHaise <Ghostrex2@gmail.com>
2023-03-11 18:47:17 +0300

    Connect Route to server :

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from "express"
 import * as dotenv from "dotenv"
 import cors from "cors"
 import connectDB from "./mongodb/connect.js";
+import userRouter from "./routes/user.routes.js"
+import propertyRouter from "./routes/property.routes.js"
+
 
 dotenv.config();
 
@@ -13,6 +16,9 @@ app.use(express.json({limit : "50mb"}));
 app.get("/",(req,res)=>{
     res.send({message : "GhosT dashboard endpoint,Hello to you"});
 })
+
+app.use("/api/v1/users",userRouter);
+app.use("/api/v1/properties",propertyRouter);
 
 const startServer = async()=>{
     try{
