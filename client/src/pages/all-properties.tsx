6bda11dfab536c35268f04456fcb7af849a8e492
GhosTHaise <Ghostrex2@gmail.com>
2023-03-12 21:10:23 +0300

    get all data from my nodejs using dataprovider and useTable

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,5 +1,5 @@
 import { Add } from '@mui/icons-material'
-import { useList } from '@pankod/refine-core'
+import { useTable } from '@pankod/refine-core'
 import { Box,Stack,Typography } from '@pankod/refine-mui'
 import { useNavigate } from '@pankod/refine-react-router-v6'
 import { PropertyCard,CustomButton } from 'components'
@@ -8,6 +8,17 @@ type Props = {}
 
 const AllProperties = (props: Props) => {
   const navigate = useNavigate();
+  const {
+    tableQueryResult : {
+        data,
+        isLoading,
+        isError
+    }
+  } = useTable();
+  
+  const allProperties = data?.data ?? [];
+  if(isLoading) return <Typography>Loading...</Typography>
+  if(isError) return <Typography>Error...</Typography>
   return (
     <Box>
       <Stack
@@ -31,6 +42,15 @@ const AllProperties = (props: Props) => {
             icon={<Add />}
           />
       </Stack>
+      <Box 
+      mt="20px" 
+      style={{
+        display : "flex",
+        flexWrap : "wrap",
+        gap : 3
+      }}>
+        
+      </Box>
     </Box>
   )
 }
--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -12,7 +12,15 @@ cloudinary.config({
     api_secret : process.env.CLOUDINARY_API_SECRET
 })
 
-const getAllProperties = async(req,res) => {};
+const getAllProperties = async(req,res) => {
+    try {
+        const properties = await Property.find({}).limit(req.query._end);
+        res.status(200).json(properties)
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
+};
+
 const getPropertyDetail = async(req,res) => {};
 
 const createProperty = async(req,res) => {
