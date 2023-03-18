06268d18b32edd8e4e0f7aa7db67e1c0db5e847e
GhosTHaise <Ghostrex2@gmail.com>
2023-03-18 21:23:23 +0300

    Create update profile controller function

--- server/routes/property.routes.js
+++ server/routes/property.routes.js
@@ -12,7 +12,7 @@ const Router = express.Router();
 Router.route("/").get(getAllProperties);
 Router.route("/:id").get(getPropertyDetail);
 Router.route("/").post(createProperty);
-Router.route("/").patch(updateProperty);
+Router.route("/:id").patch(updateProperty);
 Router.route("/:id").delete(deleteProperty);
 
 export default Router;
