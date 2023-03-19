1c3d1b6adcc3a009bce441def1e64f178b3ba1bb
GhosTHaise <Ghostrex2@gmail.com>
2023-03-19 19:44:05 +0300

    Build Get all property controller function

--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useList } from "@pankod/refine-core";
+import { Box , Typography } from "@pankod/refine-mui"
+import { AgentCard } from "components"; 
 
 type Props = {}
 
-const agent = (props: Props) => {
+const Agent = (props: Props) => {
+  const { data,isLoading,isError} = useList({
+    resource : "users"
+  });
+
+  const allAgents = data?.data ?? [];
+
+  if(isLoading) return <div>Loading ...</div>
+  if(isError) return <div>Error ...</div>
   return (
     <div>agent</div>
   )
 }
 
-export default agent
+export default Agent
